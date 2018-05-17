2be54289f2706bed0ff1429cf7ec1d7f02843bd6
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-17 15:13:34 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -8,11 +8,11 @@ function lastImageSelector () {
 }
 
 function ninjaBabySelector () {
-  return ('#baby-ninja');
+  return $('#baby-ninja');
 }
 
 function divSelector() {
-  return ('.pics');
+  return $('.pics');
 }
 
 function firstListItem() {
